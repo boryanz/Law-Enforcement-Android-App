40334d9b93ad321c90ea121cb73cb7b9554cf2e1
bjanevsk <boris.janevski@netcetera.com>
2024-12-09 12:58:55 +0100

    setting up template

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -40,7 +41,21 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.kotlinx.serialization.json)
+//    implementation(libs.retrofit.core)
+//    implementation(libs.retrofit.bom)
+//    implementation(libs.coil.kt)
+//    implementation(libs.coil.kt.compose)
+//    implementation(libs.okhttp)
+//    implementation(libs.okhttp.logging)
+//    implementation(libs.koin.bom)
+//    implementation(libs.koin.android)
+    implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.compose.material.iconsExtended)
+    implementation(libs.gson)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
---
+++ app/src/main/java/com/example/katasampleapp/ui/screens/Screen2.kt
@@ -0,0 +1,30 @@
+package com.example.katasampleapp.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Screen2(
+    onNavigateBack: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Screen 2")
+        Spacer(modifier = Modifier.padding(vertical = 12.dp))
+        Button(onClick = onNavigateBack) {
+            Text("Go back")
+        }
+    }
+}
