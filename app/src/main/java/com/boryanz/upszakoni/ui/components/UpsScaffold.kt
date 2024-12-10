67eb9f1567e7993e4e578294ce222a680651ab1f
bjanevsk <boris.janevski@netcetera.com>
2024-12-10 20:17:49 +0100

    removed imports

--- app/src/main/java/com/boryanz/upszakoni/ui/components/UpsScaffold.kt
+++ app/src/main/java/com/boryanz/upszakoni/ui/components/UpsScaffold.kt
@@ -1,16 +1,12 @@
 package com.boryanz.upszakoni.ui.components
 
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.SearchBar
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.tooling.preview.Preview
 import com.boryanz.upszakoni.ui.theme.Base100
 import com.boryanz.upszakoni.ui.theme.BaseContent
