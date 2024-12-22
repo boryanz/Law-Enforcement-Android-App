1edcc33029a922eb9348ad6b4e96436bc8882a60
bjanevsk <boris.janevski@netcetera.com>
2024-12-22 11:19:38 +0100

    make NavigationDrawer more ui appealing

--- app/src/main/java/com/boryanz/upszakoni/ui/components/NavigationDrawer.kt
+++ app/src/main/java/com/boryanz/upszakoni/ui/components/NavigationDrawer.kt
@@ -1,11 +1,16 @@
 package com.boryanz.upszakoni.ui.components
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.DrawerValue
@@ -21,7 +26,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.boryanz.upszakoni.R
 import com.boryanz.upszakoni.data.NavigationDrawerDestination
 import com.boryanz.upszakoni.ui.theme.Base100
@@ -45,112 +52,130 @@ fun NavigationDrawer(
     ModalNavigationDrawer(
         drawerContent = {
             ModalDrawerSheet {
-                Text("УПС", modifier = Modifier.padding(16.dp))
-                HorizontalDivider()
-                Column {
-                    NavigationDrawerItem(
-                        icon = {
-                            Icon(
-                                modifier = Modifier
-                                    .height(20.dp)
-                                    .width(20.dp),
-                                painter = painterResource(R.drawable.offenses),
-                                contentDescription = null
-                            )
-                        },
-                        label = { Text(text = "Чести прекршоци") },
-                        selected = false,
-                        onClick = { onItemClicked(NavigationDrawerDestination.offenses) }
-                    )
-                    HorizontalDivider()
-                    NavigationDrawerItem(
-                        icon = {
-                            Icon(
-                                modifier = Modifier
-                                    .height(20.dp)
-                                    .width(20.dp),
-                                painter = painterResource(R.drawable.kriminal),
-                                contentDescription = null
-                            )
-                        },
-                        label = { Text(text = "Чести кривични дела") },
-                        selected = false,
-                        onClick = { onItemClicked(NavigationDrawerDestination.crimes) }
-                    )
-                    HorizontalDivider()
-                    NavigationDrawerItem(
-                        icon = {
-                            Icon(
-                                modifier = Modifier
-                                    .height(20.dp)
-                                    .width(20.dp),
-                                painter = painterResource(R.drawable.police),
-                                contentDescription = null
-                            )
-                        },
-                        label = { Text(text = "Твои овластувања") },
-                        selected = false,
-                        onClick = { onItemClicked(NavigationDrawerDestination.authorities) }
-                    )
-                    HorizontalDivider()
-                    NavigationDrawerItem(
-                        icon = {
-                            Icon(
-                                modifier = Modifier
-                                    .height(20.dp)
-                                    .width(20.dp),
-                                painter = painterResource(R.drawable.question_solid),
-                                contentDescription = null
-                            )
-                        },
-                        label = { Text(text = "Прашања за службена белешка") },
-                        selected = false,
-                        onClick = { onItemClicked(NavigationDrawerDestination.writing_guide) }
-                    )
-                    HorizontalDivider()
-                    NavigationDrawerItem(
-                        icon = {
-                            Icon(
-                                modifier = Modifier
-                                    .height(20.dp)
-                                    .width(20.dp),
-                                painter = painterResource(R.drawable.prekrsok),
-                                contentDescription = null
-                            )
-                        },
-                        label = { Text(text = "Потраги и исчезнати лица") },
-                        selected = false,
-                        onClick = { onItemClicked(NavigationDrawerDestination.wanted_criminals) }
-                    )
-                    HorizontalDivider()
-                    NavigationDrawerItem(
-                        icon = {
-                            Icon(
-                                modifier = Modifier
-                                    .height(20.dp)
-                                    .width(20.dp),
-                                painter = painterResource(R.drawable.vesti),
-                                contentDescription = null
-                            )
-                        },
-                        label = { Text(text = "Дневен билтен") },
-                        selected = false,
-                        onClick = { onItemClicked(NavigationDrawerDestination.daily_news) }
-                    )
-                    HorizontalDivider()
-                    NavigationDrawerItem(
-                        icon = {
-                            Icon(
-                                modifier = Modifier
-                                    .height(20.dp)
-                                    .width(20.dp),
-                                painter = painterResource(R.drawable.phone_solid),
-                                contentDescription = null
-                            )
-                        },
-                        label = { Text(text = "Броеви од ПС - СВР Скопје") },
-                        selected = false,
-                        onClick = { onItemClicked(NavigationDrawerDestination.phone_numbers) }
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 16.dp)
+                        .verticalScroll(rememberScrollState()),
+                    verticalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    Column {
+                        Spacer.Vertical(12.dp)
+                        Text("УПС", modifier = Modifier.padding(16.dp))
+                        HorizontalDivider()
+                        NavigationDrawerItem(
+                            icon = {
+                                Icon(
+                                    modifier = Modifier
+                                        .height(20.dp)
+                                        .width(20.dp),
+                                    painter = painterResource(R.drawable.offenses),
+                                    contentDescription = null
+                                )
+                            },
+                            label = { Text(text = "Чести прекршоци") },
+                            selected = false,
+                            onClick = { onItemClicked(NavigationDrawerDestination.offenses) }
+                        )
+                        HorizontalDivider()
+                        NavigationDrawerItem(
+                            icon = {
+                                Icon(
+                                    modifier = Modifier
+                                        .height(20.dp)
+                                        .width(20.dp),
+                                    painter = painterResource(R.drawable.kriminal),
+                                    contentDescription = null
+                                )
+                            },
+                            label = { Text(text = "Чести кривични дела") },
+                            selected = false,
+                            onClick = { onItemClicked(NavigationDrawerDestination.crimes) }
+                        )
+                        HorizontalDivider()
+                        Spacer.Vertical(14.dp)
+                        NavigationDrawerItem(
+                            icon = {
+                                Icon(
+                                    modifier = Modifier
+                                        .height(20.dp)
+                                        .width(20.dp),
+                                    painter = painterResource(R.drawable.police),
+                                    contentDescription = null
+                                )
+                            },
+                            label = { Text(text = "Твои овластувања") },
+                            selected = false,
+                            onClick = { onItemClicked(NavigationDrawerDestination.authorities) }
+                        )
+                        HorizontalDivider()
+                        NavigationDrawerItem(
+                            icon = {
+                                Icon(
+                                    modifier = Modifier
+                                        .height(20.dp)
+                                        .width(20.dp),
+                                    painter = painterResource(R.drawable.question_solid),
+                                    contentDescription = null
+                                )
+                            },
+                            label = { Text(text = "Прашања за службена белешка") },
+                            selected = false,
+                            onClick = { onItemClicked(NavigationDrawerDestination.writing_guide) }
+                        )
+                        HorizontalDivider()
+                        Spacer.Vertical(14.dp)
+                        NavigationDrawerItem(
+                            icon = {
+                                Icon(
+                                    modifier = Modifier
+                                        .height(20.dp)
+                                        .width(20.dp),
+                                    painter = painterResource(R.drawable.prekrsok),
+                                    contentDescription = null
+                                )
+                            },
+                            label = { Text(text = "Потраги и исчезнати лица") },
+                            selected = false,
+                            onClick = { onItemClicked(NavigationDrawerDestination.wanted_criminals) }
+                        )
+                        HorizontalDivider()
+                        NavigationDrawerItem(
+                            icon = {
+                                Icon(
+                                    modifier = Modifier
+                                        .height(20.dp)
+                                        .width(20.dp),
+                                    painter = painterResource(R.drawable.vesti),
+                                    contentDescription = null
+                                )
+                            },
+                            label = { Text(text = "Дневен билтен") },
+                            selected = false,
+                            onClick = { onItemClicked(NavigationDrawerDestination.daily_news) }
+                        )
+                        HorizontalDivider()
+                        Spacer.Vertical(14.dp)
+                        NavigationDrawerItem(
+                            icon = {
+                                Icon(
+                                    modifier = Modifier
+                                        .height(20.dp)
+                                        .width(20.dp),
+                                    painter = painterResource(R.drawable.phone_solid),
+                                    contentDescription = null
+                                )
+                            },
+                            label = { Text(text = "Броеви од ПС - СВР Скопје") },
+                            selected = false,
+                            onClick = { onItemClicked(NavigationDrawerDestination.phone_numbers) }
+                        )
+                    }
+                    Text(
+                        modifier = Modifier.fillMaxWidth().padding(start = 12.dp),
+                        text = "Имаш нов пречистен текст или забелешки? Испрати на boryans.co@gmail.com",
+                        textAlign = TextAlign.Start,
+                        fontSize = 14.sp
                     )
                 }
             }
