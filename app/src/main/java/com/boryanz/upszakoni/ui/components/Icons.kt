c8f05ead89852f8dfd86777a596438306da61b22
bjanevsk <boris.janevski@netcetera.com>
2024-12-26 07:33:27 +0100

    removed unused imports

--- app/src/main/java/com/boryanz/upszakoni/ui/components/Icons.kt
+++ app/src/main/java/com/boryanz/upszakoni/ui/components/Icons.kt
@@ -4,9 +4,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.boryanz.upszakoni.ui.theme.Base100
 
