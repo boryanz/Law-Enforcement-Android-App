08d9735e6738a9e88618826f133feb9ff269fdd6
boryanz <boryans.co@gmail.com>
2026-03-12 14:56:37 +0100

    small refactor in LawsViewModel.kt

--- app/src/main/java/com/boryanz/upszakoni/domain/laws/LawsRepository.kt
+++ app/src/main/java/com/boryanz/upszakoni/domain/laws/LawsRepository.kt
@@ -7,30 +7,29 @@ import com.boryanz.upszakoni.domain.laws.model.Law
 import com.boryanz.upszakoni.domain.safeApi
 
 class LawsRepository(
-  private val api: LawApiService,
-  private val pdfCache: PdfCache,
-) : LawsProvider {
+    private val api: LawApiService,
+    private val pdfCache: PdfCache,
+) : LawsRepo {
 
-  private val cachedLaws: MutableList<Law> = mutableListOf()
+    private val cachedLaws: MutableList<Law> = mutableListOf()
 
-  override suspend fun getLaws(): Result<List<Law>> {
-    return if (cachedLaws.isNotEmpty()) {
-      Result.Success(cachedLaws)
-    } else {
-      safeApi {
-        api.getLaws().map { it.toDomain() }.also {
-          cachedLaws.addAll(it)
+    override suspend fun getLaws(): Result<List<Law>> {
+        return if (cachedLaws.isNotEmpty()) Result.Success(cachedLaws)
+        else {
+            safeApi {
+                api.getLaws()
+                    .map { it.toDomain() }
+                    .also { cachedLaws.addAll(it) }
+            }
         }
-      }
     }
-  }
 
-  override suspend fun getLaw(fileName: String, id: String): Result<String> {
-    pdfCache.getPdfPath(fileName)?.let { return Result.Success(it) }
+    override suspend fun getLaw(fileName: String, id: String): Result<String> {
+        pdfCache.getPdfPath(fileName)?.let { return Result.Success(it) }
 
-    return safeApi {
-      val responseBody = api.downloadPdf(id)
-      pdfCache.saveAndGet(fileName, responseBody)
+        return safeApi {
+            val responseBody = api.downloadPdf(id)
+            pdfCache.saveAndGet(fileName, responseBody)
+        }
     }
-  }
 }
